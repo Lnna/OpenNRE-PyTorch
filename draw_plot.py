d413866b381bb282a47afb73118e48c63fae0b2d
lnn <lnn@lnn-X411>
2019-10-13 11:04:00 +0800

    extra

--- draw_plot.py
+++ draw_plot.py
@@ -7,7 +7,7 @@ import sys
 import os
 
 # result_dir = './mnre_data/176rels_data/f189_test_result'
-result_dir = '/media/sda1/nana/opennre-pytorch/mnre_data/176rels_data/bert_origin_test_result'
+result_dir = '/media/sda1/nana/opennre-pytorch/mnre_data/176rels_data/f189_test_result'
 
 def main():
 	models = sys.argv[1:]
